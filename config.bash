4d6d6f7f4264f555f67a3cf46ca5431a58d5fdc4
Akihiro TAKASE <42milez@gmail.com>
2020-07-24 17:57:13 +0900

    initial commit

---
+++ config.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Abort script at first error, when a command exits with non-zero status
+set -e
+
+# Attempt to use undefined variable outputs error message, and forces an exit.
+# set -u
+
+# Print each command to stdout before executing it
+# set -v
+
+# Similar to -v, but expands commands
+# set -x
